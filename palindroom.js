ea36ad9607f6f4546eb66a9436568f197e42352c
ahmedibrahimhassan654 <ahmedibrahimhassan654@gmail.com>
2021-01-22 00:31:50 +0200

    plained rom question

--- palindroom.js
+++ palindroom.js
@@ -1,2 +1,27 @@
 //find palindroom
+//if 'hello' after reverse='olleh' it is diffrent than original so it return false 
 
+//palindroo('abba) reverse='abba'=== return true
+
+
+const palindroom=(word)=>{
+    /*
+    1-change string to array//split()
+    2-reverse string
+    3-check if it is equal to original
+     */
+const wordToarray=word.split('')
+console.log('wordToarray',wordToarray);
+  const reverse=wordToarray.reverse()
+
+  console.log('reverse string inside array',reverse);
+  const original=reverse.join('')
+  console.log(word===original);
+
+
+
+  
+
+}
+
+palindroom('abba')
