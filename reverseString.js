099e880dc4c673f2b2dd3a85737346a2cf8b95f0
ahmedibrahimhassan654 <ahmedibrahimhassan654@gmail.com>
2021-01-21 23:03:28 +0200

    reverse string

---
+++ reverseString.js
@@ -0,0 +1,10 @@
+//you have input string 'hello' reverse it 
+const reverse=(str)=>{
+let array=str.split('')
+const reverse=array.reverse()
+const back=reverse.join('')
+console.log(back);
+}
+
+
+reverse('hello' )
